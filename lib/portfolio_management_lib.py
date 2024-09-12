98fe524069660e1b24229e7260507555f347fcae
Joohan Jin <wngks3146@naver.com>
2024-09-12 22:44:53 +0800

    feat: market capitailization index and return have been implemented

--- lib/portfolio_management_lib.py
+++ lib/portfolio_management_lib.py
@@ -525,3 +525,37 @@ def gmv(cov):
     """
     n = cov.shape[0]
     return msr(np.repeat(1, n), cov, 0)
+
+
+def get_ind_nfirms():
+    """
+    Load and format the Ken French 30 Industry Portfolios average number of Firms.
+    """
+    ind = pd.read_csv("data/ind30_m_nfirms.csv", header = 0, index_col = 0)
+    ind.index = pd.to_datetime(ind.index, format = "%Y%m").to_period("M")
+    ind.columns = ind.columns.str.strip()
+    return ind
+
+
+def get_ind_size():
+    """
+    Load and format the Ken French 30 Industry Portfolio Average Size (market capitalization)
+    """
+    ind = pd.read_csv("data/ind30_m_size.csv", header = 0, index_col = 0)
+    ind.index = pd.to_datetime(ind.index, format = "%Y%m").to_period("M")
+    ind.columns = ind.columns.str.strip()
+    return ind
+
+
+def get_total_market_index_returns():
+    """
+    Load the 30 industry portfolio data and derive the returns of a capweighted total market index.
+    """
+    ind_nfirms = get_ind_nfirms()
+    ind_size = get_ind_size()
+    ind_return = get_ind_returns()
+    ind_mktcap = ind_nfirms * ind_size
+    total_mktcap = ind_mktcap.sum(axis = 1)
+    ind_capweight = ind_mktcap.divide(total_mktcap, axis = 0)
+    total_market_return = (ind_capweight * ind_return).sum(axis = 1)
+    return total_market_return

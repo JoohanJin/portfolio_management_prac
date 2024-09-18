e2489d1785a36f9897e8827d617b8aafd4ae09c4
Joohan Jin <wngks3146@naver.com>
2024-09-18 17:12:52 +0800

    feat: implemented the chatper 17 simulating intereate rate changes and Funding Rate changes using CIR Model

--- lib/portfolio_management_lib.py
+++ lib/portfolio_management_lib.py
@@ -1,4 +1,5 @@
 # library import
+import math
 from typing import Union, Literal
 import pandas as pd
 import numpy as np
@@ -778,3 +779,107 @@ def funding_ratio(
     Computes the funding ratio of a series of liabilities based on an interest rate and current value of assets.
     """
     return assets / pv(liabilities, r)
+
+
+def inst_to_ann(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate.
+    return exp(x) - 1
+    """
+    return np.expm1(r)
+
+
+def ann_to_inst(r):
+    """
+    Convert an annualized interest rate to an annual interest rate
+    """
+    return np.log1p(r)
+
+
+def cir(
+    n_years = 10,
+    n_scenarios = 1,
+    a = 0.05,
+    b = 0.03,
+    sigma = 0.05,
+    steps_per_year = 12,
+    r_0 = None
+):
+    """
+    Generate random interest rate evolution over time using the CIR model.
+    b and r_0 are assumed to be teh annualzied rates, not the short rate
+    and the returned values are the annualized rates as well.
+    """
+    if r_0 is None: r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1 / steps_per_year
+    num_steps = int(n_years * steps_per_year) + 1 # because n_years might be a float
+
+    shock = np.random.normal(0, scale = np.sqrt(dt), size = (num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+    for step in range(1, num_steps):
+        r_t = rates[step - 1]
+        d_r_t = a * (b - r_t) * dt + sigma * np.sqrt(r_t) * shock[step]
+        rates[step] = abs(r_t + d_r_t) # just in case of roundoff errors going negative
+
+    return pd.DataFrame(data = inst_to_ann(rates), index = range(num_steps))
+
+
+def cir_zero_coupon_bond(
+    n_years = 10,
+    n_scenarios = 1,
+    a = 0.05,
+    b = 0.03,
+    sigma = 0.05,
+    steps_per_year = 12,
+    r_0 = None,
+):
+    """
+    Generate random intereat rate evolution over time using CIR model.
+    b and r_0 are assumed to be the anualized rates, not the short rate.
+    and the returned values are tha annualzied rates as well.
+    """
+    if r_0 is None: r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1 / steps_per_year
+    num_steps = int(n_years * steps_per_year) + 1 # because n_years might be a float
+
+    shock = np.random.normal(
+        0,
+        scale = np.sqrt(dt),
+        size = (num_steps, n_scenarios)
+    )
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    # For Price Generation
+    h = math.sqrt(a ** 2 + 2 * sigma ** 2)
+    prices = np.empty_like(shock)
+
+    def price(ttm, r):
+        _A = ((2 * h * math.exp((h + a) * ttm / 2)) / (2 * h + (h + a) * (math.exp(h * ttm) - 1))) ** (2 * a * b / sigma ** 2)
+        _B = (2 * (math.exp(ttm * h) - 1)) / (2 * h + (a + h) * (math.exp(ttm * h) - 1))
+        _P = _A * np.exp(-_B * r)
+        return _P
+    
+    prices[0] = price(n_years, r_0)
+
+
+    for step in range(1, num_steps):
+        r_t = rates[step - 1]
+        d_r_t = a * (b - r_t) * dt + sigma * np.sqrt(r_t) * shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well
+        prices[step] = price(
+            ttm = n_years - step * dt,
+            r = rates[step],
+        )
+
+    rates = pd.DataFrame(data = inst_to_ann(rates), index = range(num_steps))
+    # For prices
+    prices = pd.DataFrame(
+        data = prices,
+        index = range(num_steps),
+    )
+    return rates, prices
